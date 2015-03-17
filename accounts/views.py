53084ebff27acf2859a47e32855191bb3d230f34
Kazbek <nmb.ten@gmail.com>
2015-03-17 12:19:12 +0300

    Bootstrap styles added to profile page.

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from django.conf import settings
+from django.contrib import messages
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.views.generic.edit import UpdateView
@@ -20,5 +21,16 @@ class UserUpdate(UpdateView):
         kwargs['available_backends'] = load_backends(settings.AUTHENTICATION_BACKENDS)
         return kwargs
 
+    def form_invalid(self, form):
+        """
+        Called if a form is invalid. Re-renders the context data with the
+        data-filled forms and errors.
+        """
+        messages.add_message(self.request, messages.ERROR, 'Submitted form is invalid.')
+        return super(UserUpdate, self).form_invalid(form)
+
     def get_success_url(self, *args, **kwargs):
-        return reverse('accounts:profile')
+        messages.add_message(self.request, messages.SUCCESS, 'Profile saved.')
+        if 'save-and-continue' in self.request.POST:
+            return reverse('accounts:profile')
+        return reverse('index')
